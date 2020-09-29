6f6acd2deee2484a4fca528598ad6567774e8bca
BartSakowski <bart.sakowski@gmail.com>
2020-09-28 23:58:16 -0500

    remove useStyles

--- FrontEnd/src/components/BeerCard.js
+++ FrontEnd/src/components/BeerCard.js
@@ -2,36 +2,23 @@ import React from 'react';
 
 import { Card, CardHeader, CardMedia, CardContent, CardActions, Typography } from '@material-ui/core';
 
-// const useStyles = makeStyles({
-//   // root: { //the entire card
-//   //   width: 1000,
-//   //   maxHeight: 100,
-//   //   margin: 5
-//   // },
-//   // media: { //just the image
-//   //   height: 200,
-//   //   // width: 400
-//   // },
-// });
-
 const BeerCard = props => {
   const stockImage = 'https://www.brewdog.com/media/aheadworks/store_locator/19/intro_logo/Brewdog.jpg'
 
+  const displayImage = props.card.image_url ? require('' + props.card.image_url) : stockImage
 
   return(
-    <div >
-
+    <div >  
     <Card style={{ height: 800, width: 300, margin: 10, boxShadow: "1px 3px 1px #9E9E9E" }} >
     {console.log("BeerCard", props)}
     
       <CardHeader
         title={props.card.name}
       />
-      <CardMedia  
-        style={{ height: 200 }}
-        image={ props.card.image ? require("" + props.card.image) : stockImage}
-        // image={require( "" + props.card.image ? props.card.image : stockImage)}
-        
+      <CardMedia
+        component="img"
+        style={{ "300px" : "10em" }}
+        image={ displayImage }
       />
       <CardContent>
         <Typography>
